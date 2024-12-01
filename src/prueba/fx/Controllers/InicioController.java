9d4b9d1416b13d62140cb8acaf6b798028a821c2
Jose F7 <recuerdaporquelohaces@gmail.com>
2024-11-30 23:18:52 -0500

    Version 0.19 Adjusting inicio Controller

--- src/prueba/fx/Controllers/InicioController.java
+++ src/prueba/fx/Controllers/InicioController.java
@@ -1,26 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package prueba.fx.Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 
 /**
- * FXML Controller class
- *
- * @author Usuario
+ * FXML Controller class for the main store interface
  */
 public class InicioController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Button btnProductos;
+    @FXML
+    private Button btnCarrito;
+    @FXML
+    private Button btnListaDeseos;
+    @FXML
+    private Button btnHistorial;
+    @FXML
+    private StackPane contentArea;
+    @FXML
+    private ImageView imgInicio;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        configureButtons();
+    }
+
+    private void configureButtons() {
+        btnProductos.setOnAction(e -> loadView("productos.fxml"));
+        btnCarrito.setOnAction(e -> loadView("carrito.fxml"));
+        btnListaDeseos.setOnAction(e -> loadView("listaDeseos.fxml"));
+        btnHistorial.setOnAction(e -> loadView("historial.fxml"));
+    }
+
+    private void loadView(String fxmlFile) {
+        try {
+            StackPane newView = javafx.fxml.FXMLLoader.load(getClass().getResource("/prueba/fx/Views/" + fxmlFile));
+            contentArea.getChildren().clear();
+            contentArea.getChildren().add(newView);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
